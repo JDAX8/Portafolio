8288f1a7799b2547a53771bb41ceb533a4530470
J-David8 <juan.valencia@pacific-hs.com>
2026-01-20 12:12:02 -0500

    Add new images and update database

--- src/data/projectsData.jsx
+++ src/data/projectsData.jsx
@@ -1,28 +1,44 @@
 // src/data/projects.js
-import Alien from '/photos/Alien.png'
-import Catnap from '/photos/Catnap.png'
-import Robbie from '/photos/Robbie.jpg'
+import Anime from '/photos/Anime.png'
+import Bancoldex from '/photos/Bancolde.png'
+import EDDA from '/photos/EDDA.png'
+import Rappi from '/photos/Rappi.jpg'
+import Teapoyamos from '/photos/Teapoyamo.png'
 
 export const projectsData = [
-  {
-    id: 1,
-    name: 'Alien',
-    projectImg: Alien,
-    description: 'Este es un alienígena que modelé en Blender, explorando formas orgánicas y detalles en la textura de la piel para darle un aspecto inquietante...',
-    projectLink: 'https://www.behance.net/gallery/217400185/Space-alien'
-  },
-  {
-    id: 2,
-    name: 'Catnap',
-    projectImg: Catnap,
-    description: 'Este es un render en 3D de Catnap, un personaje que modelé en Blender, enfocándome en texturas realistas y una iluminación cinematográfica...',
-    projectLink: 'https://www.behance.net/gallery/217117169/CATNAP-3D-(Poppy-playtime)'
-  },
-  {
-    id: 3,
-    name: 'Robbie 3D',
-    projectImg: Robbie,
-    description: 'Este es un robot 3D que modelé en Blender para explorar el uso de nodos y técnicas básicas de modelado...',
-    projectLink: 'https://www.behance.net/gallery/217118717/Robbie'
-  }
-];
+	{
+		id: 1,
+		name: 'Rappi',
+		projectImg: Rappi,
+		description: 'Este fue un proyecto centrado en el diseño UI y UX de un nuevo producto, tipo minijuego para la aplicación de Rappi',
+		projectLink: 'https://www.behance.net/gallery/186101687/Rappi-Quest-Final',
+	},
+	{
+		id: 2,
+		name: 'Teapoyamos',
+		projectImg: Teapoyamos,
+		description: 'Este fue un rediseño a una página web, de la fundación Teapoyamos.',
+		projectLink: 'https://www.behance.net/gallery/172971959/TEApoyamos',
+	},
+	{
+		id: 3,
+		name: 'Bancoldex',
+		projectImg: Bancoldex,
+		description: 'Este es un proyecto centrado en diseño UX y UI, para un producto que se necesitaba en el banco Bancoldex',
+		projectLink: 'https://www.behance.net/gallery/213360305/Anticipo-de-ventas-Bancoldex',
+	},
+	{
+		id: 4,
+		name: 'AnimelistRecomendation',
+		projectImg: Anime,
+		description: 'Este es un proyecto centrado en diseño UX, UI y programación front-end, con el fin de crear un sistema de recomendación anime en base a la página MyAnimeList',
+		projectLink: 'https://www.behance.net/gallery/213608119/MyAnimeList-Recomendation-System',
+	},
+	{
+		id: 5,
+		name: 'EDDA',
+		projectImg: EDDA,
+		description: 'Este es un proyecto centrado en diseño UI y UX, con el fin de crear una aplicación con la que se pueda revesar canchas de fútbol',
+		projectLink: 'https://www.behance.net/gallery/183036877/EDDA',
+	},
+]
